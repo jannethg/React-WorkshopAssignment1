75b03b8e71e07fc3aadccde4cae34865bd376324
Janneth Gutierrez <jgutier1@gmail.com>
2023-08-10 00:45:50 -0400

    Example file for learning purpose only!

--- src/features/campsites/examples.js
+++ src/features/campsites/examples.js
@@ -1,15 +1,15 @@
 // Examples of destructuring
 
-const [banana, apple] = ['yellow', 'red'];
+//const [banana, apple] = ['yellow', 'red'];
 // the variable banana will contain the value 'yellow'
 // the variable apple will contain the value 'red'
 
-const transformNum = (num) => [num * 2, num + 2];
-const [numTimesTwo, numPlusTwo] = transformNum(10)
+//const transformNum = (num) => [num * 2, num + 2];
+//const [numTimesTwo, numPlusTwo] = transformNum(10)
 // the variable numTimesTwo will contain the value 20
 // the variable numPlus Two will contain the value 12
 
-const [state, setState] = useState(initialState);
+//const [state, setState] = useState(initialState);
 // array destructuring is used to get the state and setState values
 // from the useState hook -- you will learn about this soon!
 
