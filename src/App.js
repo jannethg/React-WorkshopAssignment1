4f4bfa04b65e4c4ba9c33e20142118784a0b9c07
Janneth Gutierrez <jgutier1@gmail.com>
2023-08-11 19:34:47 -0400

    React 2: About Us Page and breadcrumbs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
                 path='directory/:campsiteId'
                 element={<CampsiteDetailPage />}
           />   
-         
+          <Route path='about' element={<AboutPage />} />         
       </Routes>
       <Footer />
     </div>
