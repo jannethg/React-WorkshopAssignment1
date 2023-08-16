13ae4a0bff8c13b4503468acf0163ce0034a1ad8
Janneth Gutierrez <jgutier1@gmail.com>
2023-08-16 02:36:08 -0400

    Week 3:  Weekly Assignment Comment Form
    Formik form

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -30,7 +30,52 @@ const CommentForm = ({campsiteId}) => {
                 <ModalHeader toggle={() => setModalOpen(false)} >
                     Add Comment
                 </ModalHeader>
-                <ModalBody>campsite: {campsiteId}</ModalBody>
+                <Formik
+                    initialValues={{
+                    rating: '',
+                    author: '',
+                    commentText: ''                    
+                    }}
+                    onSubmit={handleSubmit}
+                >
+                    <Form>
+                        <FormGroup>
+                            <Label htmlFor='rating'>Rating</Label>
+                                <Field
+                                    name='rating'
+                                    as='select'
+                                    className='form-control'
+                                >
+                                    <option>Select...</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Field>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor='author'>Your Name</Label>
+                            <Field
+                                name='author'
+                                placeholder='Your Name'
+                                className='form-control'
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor='commentText'>Comment</Label>
+                            <Field
+                                name='commentText'
+                                as='textarea'
+                                rows='12'
+                                className='form-control'
+                            />
+                        </FormGroup>
+                        <Button type='submit' color='primary'>
+                            Submit
+                        </Button>
+                    </Form>
+                </Formik>
             </Modal>
            
         </>
