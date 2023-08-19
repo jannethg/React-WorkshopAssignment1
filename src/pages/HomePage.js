8c39bc5829cf8d44c8dfb924e188a3d7dd98c109
Janneth Gutierrez <jgutier1@gmail.com>
2023-08-19 16:09:07 -0400

    wk3 test 123

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
     return (
         <Container>
         <SubHeader current='Home' />
-            <DisplayList />
+            <DisplayList />teest 1;lkj;skjdf;l kdsjf;lds kjf
         </Container>
 
     );
