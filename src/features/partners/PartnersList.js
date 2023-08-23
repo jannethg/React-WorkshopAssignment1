458715f1b7f648ed32e019a7814cd4c94ddfd8ec
Janneth Gutierrez <jgutier1@gmail.com>
2023-08-23 13:25:00 -0400

    Week 4:  Update Redux: Slices and Selectors

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,9 +1,11 @@
+import { useSelector } from 'react-redux';
 import { Col } from 'reactstrap';
 import Partner from './Partner';
 import { selectAllPartners } from './partnersSlice';
 
 const PartnersList = () => {
-    const partners = selectAllPartners();
+    const partners = useSelector(selectAllPartners);  //we pass it to the useSelector hook as callback
+                                                      //all we are passing is the name of the fucntion                  
 
     return (
         <Col className='mt-4'>
