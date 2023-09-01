4743e548103b2f4994c276f64b920f685e159e26
Janneth Gutierrez <jgutier1@gmail.com>
2023-09-01 16:04:57 -0400

    Week 5 Excercise adding Redux async and promise function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
@@ -7,10 +9,16 @@ import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
-import testcode from './testcode';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className="App">    
       <Header /> 
